788efb32f53d4debfb14c43a41f2c1bbc97f6ebc
Huge Letters <ckuka71@gmail.com>
2023-05-19 16:04:28 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: { "header-height": "3.5rem" },
+      colors: {
+        theme: {
+          red: "hsl(355, 60%, 50%)",
+          blue: "hsl(222, 29%, 38%)",
+          gray: "hsl(212, 15%, 22%)",
+          "gray-light": "hsl(210, 5%, 24%)",
+          "gray-dark": "hsl(270, 4%, 9%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
