ae946744f825d35230258792b227ef4b98b8785e
Huge Letters <ckuka71@gmail.com>
2023-05-22 12:06:44 +0200

    added lambda function on vercel to post likes to projects

--- backend/api/rating.ts
+++ backend/api/rating.ts
@@ -1,7 +1,63 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import type { CollectionEntry } from "astro:content";
+import { z } from "zod";
+import fs from "fs/promises";
 
 export default function (request: VercelRequest, response: VercelResponse) {
-  response.status(200).json({
-    body: "rating received!",
+  response.setHeader(
+    "Access-Control-Allow-Origin",
+    process.env.VERCEL_ENV === "development" ? "http://localhost:3000" : "https://hugeletters.org"
+  );
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  switch (request.method) {
+    case "GET":
+      return GET(request, response);
+    case "POST":
+      return POST(request, response);
+    case "OPTIONS":
+      return response.send("");
+    default:
+      return response.status(401).send({ error: "Invalid request" });
+  }
+}
+
+function GET(request: VercelRequest, response: VercelResponse) {
+  return fs.readFile("./data/rating.json", "utf-8").then((file) => {
+    const ratings = JSON.parse(file || "{}");
+    return response.json(ratings);
   });
+}
+
+export type postBody = {
+  id: CollectionEntry<"projects">["id"];
+  like: boolean;
+};
+const postBodySchema = z.object({
+  id: z.enum(
+    (await fs
+      .readdir("../src/content/projects")
+      .then((x) => x.map((id) => id.replace(/\.json/, "")))) as readonly [string, ...string[]]
+  ),
+  like: z.boolean(),
+});
+function POST(request: VercelRequest, response: VercelResponse) {
+  const vote = postBodySchema.parse(request.body);
+  return fs
+    .readFile("./data/rating.json", "utf-8")
+    .then((file) => {
+      const ratings = JSON.parse(file || "{}") as Record<string, number>;
+      const voteCount = Math.max(0, (ratings[vote.id] ?? 0) + (vote.like ? 1 : -1));
+      fs.writeFile(
+        "./data/rating.json",
+        JSON.stringify(Object.assign(ratings, { [vote.id]: voteCount }))
+      );
+    })
+    .then(() => {
+      response.send("");
+    });
 }
---
+++ src/components/likeButton.tsx
@@ -0,0 +1,71 @@
+import type { CollectionEntry } from "astro:content";
+import type { postBody } from "backend/api/rating";
+import { useLayoutEffect, useState } from "react";
+
+export default function LikeButton({ id }: { id: CollectionEntry<"projects">["id"] }) {
+  const [liked, setLiked] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  useLayoutEffect(() => {
+    if (localStorage.getItem(id) === "true") setLiked(true);
+    fetch(API_ENDPOINT)
+      .then((r) => r.json())
+      .then(console.log);
+  }, []);
+
+  function handleLikeClick() {
+    const like = !liked;
+    setDisabled(true);
+    fetch(API_ENDPOINT, {
+      body: JSON.stringify({ id, like } satisfies postBody),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          return console.error(response.statusText);
+        }
+        setLiked(like);
+        localStorage.setItem(id, like.toString());
+        setDisabled(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setDisabled(false);
+      });
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      data-liked={liked}
+      className="absolute right-2 h-8 w-fit text-3xl duration-300 disabled:text-neutral-600 hover:[&:not(:disabled)]:text-red-300 data-[liked=true]:[&:not(:disabled)]:text-red-500"
+      onClick={handleLikeClick}
+    >
+      <svg
+        version="1.1"
+        id="Uploaded to svgrepo.com"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="800px"
+        height="800px"
+        viewBox="0 0 32 32"
+        xmlSpace="preserve"
+        className="h-full w-10"
+      >
+        <path
+          className="fill-current"
+          d="M21.081,6C23.752,6.031,26,8.766,26,12c0,5.106-6.47,10.969-10.001,13.593
+	C12.466,22.974,6,17.12,6,12c0-3.234,2.248-5.969,4.918-6C13.586,6.175,13.926,6.801,16,8.879C18.069,6.806,18.418,6.173,21.081,6
+	 M20.911,4.006L20.912,4C18.993,4,17.259,4.785,16,6.048C14.741,4.785,13.007,4,11.088,4l0.001,0.006C7.044,3.936,4,7.719,4,12
+	c0,8,11.938,16,11.938,16h0.124C16.062,28,28,20,28,12C28,7.713,24.951,3.936,20.911,4.006z"
+        />
+      </svg>
+    </button>
+  );
+}
+
+const API_ENDPOINT =
+  (import.meta.env.MODE === "development"
+    ? "http://localhost:3001"
+    : "https://portfolio-api-hugeletters.vercel.app/") + "/api/rating";
